16e29a5ce5992a855f629716c5a42bf73f31d7df
Dominik Stefancik <dstefancik@gmail.com>
2023-10-28 16:01:44 +0200

    Add first endpoints

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, middleware, App, HttpResponse, HttpServer, Responder};
+
+#[get("/")]
+async fn root_route() -> impl Responder {
+    HttpResponse::Ok()
+}
+
+#[get("/health")]
+async fn health() -> impl Responder {
+    HttpResponse::Ok().body("Server running")
+}
+
+#[get("/json")]
+async fn json() -> impl Responder {
+    HttpResponse::Ok().json("{name: Luke, surname: Skywalker}")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .wrap(middleware::Logger::default())
+            .service(root_route)
+            .service(health)
+            .service(json)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
