47d8b65a02e386ca3470fd6af9eed2ada907779d
benhendriks <benhendriks@protonmail.com>
2023-10-28 21:57:40 +0200

    Added serif font

--- webapp/tailwind.config.js
+++ webapp/tailwind.config.js
@@ -1,8 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+theme: {
+
+    fontFamily: {
+      'dmserif': ['DM Serif Display', 'serif'],
+      'playfair': ['Playfair Display', 'serif'] // Ensure fonts with spaces have " " surrounding it.
+    },
+  },  plugins: [],
 };
