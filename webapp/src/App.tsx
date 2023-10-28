ec30c1277339e5ef3a07884ae915d3d71ced3665
benhendriks <benhendriks@protonmail.com>
2023-10-28 14:41:08 +0200

    Added tile componet

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import Tile from './components/Tile';
 
 function App() {
   return (
     <>
       <h1 className="text-3xl font-bold">Hello world!</h1>
+      <Tile />
     </>
   );
 }
