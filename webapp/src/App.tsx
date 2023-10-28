75b8b123c0b625f92f517c9ffa96763246a00109
Sijmen Huizenga <sijmenhuizenga@gmail.com>
2023-10-28 23:39:07 +0200

    Mouse-draggable

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,11 +1,16 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Room from "./templates/Room";
 import { Piece } from "./apiclient/model";
 import { loadData } from "./apiclient/apiclient";
 import { ComputerUpgradesPage } from "./components/ComputerUpgrades";
 import { FleischeRaceGame } from "./components/FleischeRaceGame";
+import { useDraggable } from "react-use-draggable-scroll";
 
 function App() {
+  const ref =
+    useRef<HTMLDivElement>() as React.MutableRefObject<HTMLInputElement>;
+  const { events } = useDraggable(ref);
+
   const [pieces, setPieces] = useState<Piece[]>([]);
   useEffect(() => {
     loadData().then(setPieces);
@@ -16,7 +21,11 @@ function App() {
   // }
 
   return (
-    <div className="h-screen max-h-screen overflow-y-hidden overflow-x-scroll flex">
+    <div
+      className="h-screen max-h-screen overflow-y-hidden overflow-x-scroll flex"
+      {...events}
+      ref={ref}
+    >
       {pieces.map((piece) => {
         if (piece.name === "Fleisch Ski Race") {
           return <FleischeRaceGame />;
