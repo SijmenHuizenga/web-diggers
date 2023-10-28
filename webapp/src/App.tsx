4747b3fe929fe1c82378f1b7383f9e6eee9f690c
Sijmen Huizenga <sijmenhuizenga@gmail.com>
2023-10-28 15:05:05 +0200

    Room structure

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,10 +1,30 @@
-import "./App.css";
+import { ReactNode } from "react";
 
 function App() {
   return (
-    <>
-      <h1 className="text-3xl font-bold">Hello world!</h1>
-    </>
+    <div className="h-screen max-h-screen overflow-y-hidden overflow-x-scroll flex">
+      <MusicRoom />
+      <VideoRoom />
+      <WebsiteRoom />
+    </div>
+  );
+}
+
+function MusicRoom() {
+  return <Room>I am a music room</Room>;
+}
+function VideoRoom() {
+  return <Room>VideoRoom</Room>;
+}
+function WebsiteRoom() {
+  return <Room>WebsiteRoom</Room>;
+}
+
+function Room({ children }: { children: ReactNode }) {
+  return (
+    <div className="h-full -maxh-full min-w-full border-black">
+      <h1 className="text-3xl font-bold">{children}</h1>
+    </div>
   );
 }
 
