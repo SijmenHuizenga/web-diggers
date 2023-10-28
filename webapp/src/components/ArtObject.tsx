c42b80163cc9b82bf8c6e595cc855e94c8d133e5
Oleg Lavrovsky <loleg@users.noreply.github.com>
2023-10-28 22:03:08 +0200

    Styling

--- webapp/src/components/ArtObject.tsx
+++ webapp/src/components/ArtObject.tsx
@@ -1,6 +1,10 @@
 import { Piece } from "../apiclient/model";
 
+const DEFAULT_IMG = 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c7/Optical_Toy%2C_Phenakistiscope_Disc_with_Cats_and_Donkey%2C_ca._1830.gif/640px-Optical_Toy%2C_Phenakistiscope_Disc_with_Cats_and_Donkey%2C_ca._1830.gif';
+
 const ArtObject = ({ piece }: { piece: Piece }) => {
+  let imageurl = piece.screenshot ? 
+    piece.screenshot.split(' (')[1].replace(')', '') : DEFAULT_IMG;
   return (
     <div className="grid h-screen place-items-center">
       <h2 className="title text-6xl font-bold p-4">
@@ -9,7 +13,7 @@ const ArtObject = ({ piece }: { piece: Piece }) => {
       <div className="image-container">
         <img
           className="width-full z-50"
-          src="https://source.unsplash.com/random/500x500"
+          src={imageurl}
           alt="placeholder"
         />
       </div>
