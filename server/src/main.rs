afb5b5dd42967cff684a6f7184f0369bf11cb445
Dominik Stefancik <dstefancik@gmail.com>
2023-10-28 18:48:14 +0200

    Use path from the src directory

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,11 +1,11 @@
 use actix_web::{get, middleware, App, HttpResponse, HttpServer, Responder};
-use serde_json::{Value};
-use std::{fs::File};
-
-const JSON_FILE_PATH: &str = "../../data/web-diggers-alpha.json";
+use serde_json::Value;
+use std::env::current_dir;
+use std::fs::File;
+use std::path::{PathBuf};
 
 #[get("/")]
-async fn root_route() -> impl Responder {
+async fn get_root() -> impl Responder {
     HttpResponse::Ok()
 }
 
@@ -16,8 +16,14 @@ async fn get_health() -> impl Responder {
 
 #[get("/json")]
 async fn get_json() -> impl Responder {
-    let file = File::open(JSON_FILE_PATH).expect("Failed to parse JSON file");
+    let file_path: PathBuf = current_dir()
+        .expect("Error while listing current directory")
+        .parent()
+        .expect("Error while navigating to the parent")
+        .join("data/web-diggers-alpha.json");
+    let file = File::open(file_path).expect("Failed to open JSON file");
     let json_obj: Value = serde_json::from_reader(file).expect("Failed to parse JSON file");
+
     return HttpResponse::Ok().json(json_obj);
 }
 
@@ -26,7 +32,7 @@ async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
             .wrap(middleware::Logger::default())
-            .service(root_route)
+            .service(get_root)
             .service(get_health)
             .service(get_json)
     })
