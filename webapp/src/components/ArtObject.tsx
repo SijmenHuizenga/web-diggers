c7b23e08fff16ac109b2190e154259e3ae362f9e
Oleg Lavrovsky <loleg@users.noreply.github.com>
2023-10-28 21:39:40 +0200

    UI sweeter

--- webapp/src/components/ArtObject.tsx
+++ webapp/src/components/ArtObject.tsx
@@ -3,7 +3,7 @@ import { Piece } from "../apiclient/model";
 const ArtObject = ({ piece }: { piece: Piece }) => {
   return (
     <div className="grid h-screen place-items-center">
-      <h2 className="text-6xl font-bold text-white bg-amber-900 text-amber-300 p-4">
+      <h2 className="title text-6xl font-bold p-4">
         {piece.name}
       </h2>
       <div className="image-container">
